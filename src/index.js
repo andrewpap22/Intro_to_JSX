e8301b12437718a8542fc1526ee63c931ecac204
Andrew Pappas <andrewpap1997@gmail.com>
2020-10-21 18:09:05 +0300

    :construction: insert JavaScript inside the HTML that is inside the JavaScript.

--- src/index.js
+++ src/index.js
@@ -38,16 +38,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// ReactDOM.render(
+//   <div>
+//     <h1> My 3 Favourite Sport Activities </h1>
+//     <ul>
+//       <li>Weightlifting</li>
+//       <li>Callisthenics</li>
+//       <li>Basketball</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// -------- End of Challenge -------
+
+const fname = "Mirella ";
+const lname = "Matsi";
+const wholeName = fname + lname;
+const luckyNumber = 22;
+
 ReactDOM.render(
   <div>
-    <h1> My 3 Favourite Sport Activities </h1>
-    <ul>
-      <li>Weightlifting</li>
-      <li>Callisthenics</li>
-      <li>Basketball</li>
-    </ul>
+    <h1>Hello, {wholeName}!</h1>
+    <p>Your lucky number is: {luckyNumber}</p>
   </div>,
   document.getElementById("root")
 );
 
-// -------- End of Challenge -------
+/* So as we can see with the above code, JSX allows us to use
+ * javascript inside the html that is inside our javascript,
+ * by using the {} and inside them the variable that we wrote,
+ * in the beggining.
+ *
+ * Note that inside the {} you can write any valid javascript code,
+ * For example there's no need to use the variable luckyNumber and
+ * do the calculation outside the render function, we can do
+ * inside it! Or we can even generate a random number like below:
+ * {Math.floor(Math.random() * 10)} will give us 0-9 "random" numbers
+ * */
